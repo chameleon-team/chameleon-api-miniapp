c40bba05e5b9cd02cbe72a9450b24fcef1d0a5f3
wangzhiliang <wangzhiliang@didichuxing.com>
2019-03-05 11:52:22 +0800

    初始化

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import actions from './actions'
 import getters from './getters'
 import state from './state'
 import mutations from './mutations'
-import createStore from "chameleon-store";
+import createStore from "@didi/chameleon-store";
 
 export default createStore({
   actions,
