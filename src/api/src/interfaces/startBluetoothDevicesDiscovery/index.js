b1898223d054c9eb8968e0c8da14af2ff5a2997f
wangzhiliang <wangzhiliang@didichuxing.com>
2019-03-05 20:27:20 +0800

    demo

---
+++ src/api/src/interfaces/startBluetoothDevicesDiscovery/index.js
@@ -0,0 +1,22 @@
+import index from './index.interface';
+
+export default function startBluetoothDevicesDiscovery(opt = {}) {
+    const noop = (res) => {
+    };
+
+    let {
+        // 1.这里处理默认参数
+        services = '',		allowDuplicatesKey = true,		interval = '',
+        success = noop,
+        fail = noop,
+        complete = noop
+    } = opt;
+
+    index.startBluetoothDevicesDiscovery({
+        // 2.这里把参数传进去
+        services,		allowDuplicatesKey,		interval,
+        success,
+        fail,
+        complete
+    })
+}
