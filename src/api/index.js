10e7e697e66b4b2957e106cca9506f6244af8eab
wangzhiliang <wangzhiliang@didichuxing.com>
2019-03-05 21:01:16 +0800

    demo

--- src/api/index.js
+++ src/api/index.js
@@ -3,11 +3,47 @@ import navigateTo from './src/interfaces/navigateTo/index.js';
 import navigateBack from './src/interfaces/navigateBack/index.js';
 import redirectTo from './src/interfaces/redirectTo/index.js';
 import setNavigationBarTitle from './src/interfaces/setNavigationBarTitle/index.js';
+import readBLECharacteristicValue from './src/interfaces/setNavigationBarTitle/index.js';
+import onBLEConnectionStateChange from './src/interfaces/setNavigationBarTitle/index.js';
+import onBLECharacteristicValueChange from './src/interfaces/setNavigationBarTitle/index.js';
+import notifyBLECharacteristicValueChange from './src/interfaces/setNavigationBarTitle/index.js';
+import getBLEDeviceServices from './src/interfaces/setNavigationBarTitle/index.js';
+import getBLEDeviceCharacteristics from './src/interfaces/setNavigationBarTitle/index.js';
+import createBLEConnection from './src/interfaces/setNavigationBarTitle/index.js';
+import closeBLEConnection from './src/interfaces/setNavigationBarTitle/index.js';
+import writeBLECharacteristicValue from './src/interfaces/setNavigationBarTitle/index.js';
+import startBluetoothDevicesDiscovery from './src/interfaces/setNavigationBarTitle/index.js';
+import stopBluetoothDevicesDiscovery from './src/interfaces/setNavigationBarTitle/index.js';
+import openBluetoothAdapter from './src/interfaces/setNavigationBarTitle/index.js';
+import onBluetoothDeviceFound from './src/interfaces/setNavigationBarTitle/index.js';
+import onBluetoothAdapterStateChange from './src/interfaces/setNavigationBarTitle/index.js';
+import getConnectedBluetoothDevices from './src/interfaces/setNavigationBarTitle/index.js';
+import getBluetoothDevices from './src/interfaces/setNavigationBarTitle/index.js';
+import getBluetoothAdapterState from './src/interfaces/setNavigationBarTitle/index.js';
+import closeBluetoothAdapter from './src/interfaces/setNavigationBarTitle/index.js';
 
 export default {
     showToast,
     navigateTo,
     navigateBack,
     redirectTo,
-    setNavigationBarTitle
+    setNavigationBarTitle,
+    readBLECharacteristicValue,
+    onBLEConnectionStateChange,
+    onBLECharacteristicValueChange,
+    notifyBLECharacteristicValueChange,
+    getBLEDeviceServices,
+    getBLEDeviceCharacteristics,
+    createBLEConnection,
+    closeBLEConnection,
+    writeBLECharacteristicValue,
+    startBluetoothDevicesDiscovery,
+    stopBluetoothDevicesDiscovery,
+    openBluetoothAdapter,
+    onBluetoothDeviceFound,
+    onBluetoothAdapterStateChange,
+    getConnectedBluetoothDevices,
+    getBluetoothDevices,
+    getBluetoothAdapterState,
+    closeBluetoothAdapter
 };
