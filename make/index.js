96b78d2404b0df9b157de554edcee0c68192cb1e
wangzhiliang <wangzhiliang@didichuxing.com>
2019-03-05 17:03:34 +0800

    脚本处理index.js部分

--- make/index.js
+++ make/index.js
@@ -1,21 +1,27 @@
 var fs = require('fs');
 var fse = require('fs-extra');
 var path = require('path');
-var methods = require('./config.js');
+var methods = require('./methods.js');
 
-var apiInterfacesPath = path.join(__dirname, './src/api/src/interfaces');
-var templatePath = path.join(__dirname, './make/apiTemplate');
+// 文件和路径
+var projectRoot = process.cwd();
+var apiInterfacesPath = path.join(projectRoot, './src/api/src/interfaces');
+var templatePath = path.join(projectRoot, './make/apiTemplate');
 
 // 处理
-methods.forEach(method => {
+methods.forEach(item => {
+  var method = item.method;
+  var wxParam = item.wxParam;
   var methodPath = path.join(apiInterfacesPath, method);
+
   try {
     // 复制一份方法文件夹
     fse.copySync(templatePath, methodPath);
     // 替换${method}处为方法名
     relacePlaceholder(path.join(methodPath, './index.js'), method);
     relacePlaceholder(path.join(methodPath, './index.interface'), method);
-    // 替换${wxParam}处的默认参数
+    // 替换index.js里的默认参数
+    relacewWxParam(path.join(methodPath, './index.js'), wxParam);
   } catch (e) {
     console.log(e);
   }
@@ -29,6 +35,43 @@ methods.forEach(method => {
 function relacePlaceholder(filePath, method) {
   var content = fs.readFileSync(filePath).toString('utf8');
   content = content.replace(/\$\{method\}/g, method);
+  fs.writeFileSync(filePath, content, 'utf8', function (err) {
+    if (err) return console.log(err);
+  });
+}
+
+/**
+ * 处理index.js里的参数
+ * @param {String} filePath 文件路径
+ * @param {Object} wxParam 参数
+ */
+function relacewWxParam(filePath, wxParam) {
+  var content = fs.readFileSync(filePath).toString('utf8');
+
+  // 处理解构默认值
+  var wxParamDefaultText = ``;
+  wxParam.forEach((p, i) => {
+    var defaultValue = p.default;
+    if (typeof defaultValue == 'string') {
+      if (!defaultValue) {
+        defaultValue = `''`;
+      } else {
+        defaultValue = `\'${defaultValue}\'`;
+      }
+    }
+    lastFH = i == wxParam.length - 1 ? '' : '\r\t\t'
+    wxParamDefaultText = wxParamDefaultText + `${p.name} = ${defaultValue},${lastFH}`;
+  })
+  content = content.replace(/\$\{wxParamWithType\}/g, wxParamDefaultText);
+
+  // 传递解构默认值
+  var wxParamNameText = ``;
+  wxParam.forEach((p, i) => {
+    lastFH = i == wxParam.length - 1 ? '' : '\r\t\t'
+    wxParamNameText = wxParamNameText + `${p.name},${lastFH}`;
+  })
+  content = content.replace(/\$\{wxParamOnlyName\}/g, wxParamNameText);
+
   fs.writeFileSync(filePath, content, 'utf8', function (err) {
     if (err) return console.log(err);
   });
