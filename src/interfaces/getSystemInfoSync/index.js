a65a106ddf74408e08593ed4871b56371f29a31b
zhangwenxiang <zhangwenxiang@didiglobal.com>
2020-04-15 19:48:34 +0800

    0.0.15-alpha.9

--- src/interfaces/getSystemInfoSync/index.js
+++ src/interfaces/getSystemInfoSync/index.js
@@ -1,22 +1,5 @@
-import index from './index.interface';
+import index from './index.interface'
 
-export default function getSystemInfoSync(opt = {}) {
-    const noop = (res) => {
-    };
-
-    let {
-        // 1.这里处理默认参数
-        
-        success = noop,
-        fail = noop,
-        complete = noop
-    } = opt;
-
-    index.getSystemInfoSync({
-        // 2.这里把参数传进去
-        
-        success,
-        fail,
-        complete
-    })
+export default function getSystemInfoSync () {
+  return index.getSystemInfoSync()
 }
